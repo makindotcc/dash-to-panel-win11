45783601e1cd56b8b9d4629b1516d816927dcefc
makindotcc <9150636+makindotcc@users.noreply.github.com>
2025-01-13 03:03:27 +0100

    initial flake test

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Dash to panel with win 11 alike theme.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "gnome-shell-extension-dash-to-panel-win11";
+          src = ./.;
+          meta = with pkgs.lib; {
+            description = "A fork of dash-to-panel with a theme similar to win 11.";
+            license = licenses.gpl2;
+          };
+          nativeBuildInputs = with pkgs; [ buildPackages.glib ];
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/share/gnome-shell/extensions/
+            cp -r -T . $out/share/gnome-shell/extensions/dash-to-panel@makindotcc.github.com
+            runHook postInstall
+          '';
+        };
+      }
+    );
+}
